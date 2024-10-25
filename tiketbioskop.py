d413f0b63ea1b3aa7ec431f1c8a868aca4907526
daffaeka30 <daffaekaputri30@gmail.com>
2024-10-25 11:47:02 +0700

    First and Last

---
+++ tiketbioskop.py
@@ -0,0 +1,424 @@
+b = 5
+k = 5
+matriks = []
+for i in range(b):
+    matriks.append([])
+    for j in range(k):
+        matriks[i].append(i)
+matriks[0][0] = 'A1'
+matriks[0][1] = 'A2'
+matriks[0][2] = 'A3'
+matriks[0][3] = 'A4'
+matriks[0][4] = 'A5'
+matriks[1][0] = 'B1'
+matriks[1][1] = 'B2'
+matriks[1][2] = 'B3'
+matriks[1][3] = 'B4'
+matriks[1][4] = 'B5'
+matriks[2][0] = 'C1'
+matriks[2][1] = 'C2'
+matriks[2][2] = 'C3'
+matriks[2][3] = 'C4'
+matriks[2][4] = 'C5'
+matriks[3][0] = 'D1'
+matriks[3][1] = 'D2'
+matriks[3][2] = 'D3'
+matriks[3][3] = 'D4'
+matriks[3][4] = 'D5'
+matriks[4][0] = 'E1'
+matriks[4][1] = 'E2'
+matriks[4][2] = 'E3'
+matriks[4][3] = 'E4'
+matriks[4][4] = 'E5'
+
+matriks7 = []
+for i in range(b):
+    matriks7.append([])
+    for j in range(k):
+        matriks7[i].append(i)
+matriks7[0][0] = 'A1'
+matriks7[0][1] = 'A2'
+matriks7[0][2] = 'A3'
+matriks7[0][3] = 'A4'
+matriks7[0][4] = 'A5'
+matriks7[1][0] = 'B1'
+matriks7[1][1] = 'B2'
+matriks7[1][2] = 'B3'
+matriks7[1][3] = 'B4'
+matriks7[1][4] = 'B5'
+matriks7[2][0] = 'C1'
+matriks7[2][1] = 'C2'
+matriks7[2][2] = 'C3'
+matriks7[2][3] = 'C4'
+matriks7[2][4] = 'C5'
+matriks7[3][0] = 'D1'
+matriks7[3][1] = 'D2'
+matriks7[3][2] = 'D3'
+matriks7[3][3] = 'D4'
+matriks7[3][4] = 'D5'
+matriks7[4][0] = 'E1'
+matriks7[4][1] = 'E2'
+matriks7[4][2] = 'E3'
+matriks7[4][3] = 'E4'
+matriks7[4][4] = 'E5'
+
+matriks6 = []
+for i in range(b):
+    matriks6.append([])
+    for j in range(k):
+        matriks6[i].append(i)
+matriks6[0][0] = 'A1'
+matriks6[0][1] = 'A2'
+matriks6[0][2] = 'A3'
+matriks6[0][3] = 'A4'
+matriks6[0][4] = 'A5'
+matriks6[1][0] = 'B1'
+matriks6[1][1] = 'B2'
+matriks6[1][2] = 'B3'
+matriks6[1][3] = 'B4'
+matriks6[1][4] = 'B5'
+matriks6[2][0] = 'C1'
+matriks6[2][1] = 'C2'
+matriks6[2][2] = 'C3'
+matriks6[2][3] = 'C4'
+matriks6[2][4] = 'C5'
+matriks6[3][0] = 'D1'
+matriks6[3][1] = 'D2'
+matriks6[3][2] = 'D3'
+matriks6[3][3] = 'D4'
+matriks6[3][4] = 'D5'
+matriks6[4][0] = 'E1'
+matriks6[4][1] = 'E2'
+matriks6[4][2] = 'E3'
+matriks6[4][3] = 'E4'
+matriks6[4][4] = 'E5'
+
+matriks5 = []
+for i in range(b):
+    matriks5.append([])
+    for j in range(k):
+        matriks5[i].append(i)
+matriks5[0][0] = 'A1'
+matriks5[0][1] = 'A2'
+matriks5[0][2] = 'A3'
+matriks5[0][3] = 'A4'
+matriks5[0][4] = 'A5'
+matriks5[1][0] = 'B1'
+matriks5[1][1] = 'B2'
+matriks5[1][2] = 'B3'
+matriks5[1][3] = 'B4'
+matriks5[1][4] = 'B5'
+matriks5[2][0] = 'C1'
+matriks5[2][1] = 'C2'
+matriks5[2][2] = 'C3'
+matriks5[2][3] = 'C4'
+matriks5[2][4] = 'C5'
+matriks5[3][0] = 'D1'
+matriks5[3][1] = 'D2'
+matriks5[3][2] = 'D3'
+matriks5[3][3] = 'D4'
+matriks5[3][4] = 'D5'
+matriks5[4][0] = 'E1'
+matriks5[4][1] = 'E2'
+matriks5[4][2] = 'E3'
+matriks5[4][3] = 'E4'
+matriks5[4][4] = 'E5'
+
+matriks4 = []
+for i in range(b):
+    matriks4.append([])
+    for j in range(k):
+        matriks4[i].append(i)
+matriks4[0][0] = 'A1'
+matriks4[0][1] = 'A2'
+matriks4[0][2] = 'A3'
+matriks4[0][3] = 'A4'
+matriks4[0][4] = 'A5'
+matriks4[1][0] = 'B1'
+matriks4[1][1] = 'B2'
+matriks4[1][2] = 'B3'
+matriks4[1][3] = 'B4'
+matriks4[1][4] = 'B5'
+matriks4[2][0] = 'C1'
+matriks4[2][1] = 'C2'
+matriks4[2][2] = 'C3'
+matriks4[2][3] = 'C4'
+matriks4[2][4] = 'C5'
+matriks4[3][0] = 'D1'
+matriks4[3][1] = 'D2'
+matriks4[3][2] = 'D3'
+matriks4[3][3] = 'D4'
+matriks4[3][4] = 'D5'
+matriks4[4][0] = 'E1'
+matriks4[4][1] = 'E2'
+matriks4[4][2] = 'E3'
+matriks4[4][3] = 'E4'
+matriks4[4][4] = 'E5'
+
+matriks3 = []
+for i in range(b):
+    matriks3.append([])
+    for j in range(k):
+        matriks3[i].append(i)
+matriks3[0][0] = 'A1'
+matriks3[0][1] = 'A2'
+matriks3[0][2] = 'A3'
+matriks3[0][3] = 'A4'
+matriks3[0][4] = 'A5'
+matriks3[1][0] = 'B1'
+matriks3[1][1] = 'B2'
+matriks3[1][2] = 'B3'
+matriks3[1][3] = 'B4'
+matriks3[1][4] = 'B5'
+matriks3[2][0] = 'C1'
+matriks3[2][1] = 'C2'
+matriks3[2][2] = 'C3'
+matriks3[2][3] = 'C4'
+matriks3[2][4] = 'C5'
+matriks3[3][0] = 'D1'
+matriks3[3][1] = 'D2'
+matriks3[3][2] = 'D3'
+matriks3[3][3] = 'D4'
+matriks3[3][4] = 'D5'
+matriks3[4][0] = 'E1'
+matriks3[4][1] = 'E2'
+matriks3[4][2] = 'E3'
+matriks3[4][3] = 'E4'
+matriks3[4][4] = 'E5'  
+
+matriks2 = []
+for i in range(b):
+    matriks2.append([])
+    for j in range(k):
+        matriks2[i].append(i)
+matriks2[0][0] = 'A1'
+matriks2[0][1] = 'A2'
+matriks2[0][2] = 'A3'
+matriks2[0][3] = 'A4'
+matriks2[0][4] = 'A5'
+matriks2[1][0] = 'B1'
+matriks2[1][1] = 'B2'
+matriks2[1][2] = 'B3'
+matriks2[1][3] = 'B4'
+matriks2[1][4] = 'B5'
+matriks2[2][0] = 'C1'
+matriks2[2][1] = 'C2'
+matriks2[2][2] = 'C3'
+matriks2[2][3] = 'C4'
+matriks2[2][4] = 'C5'
+matriks2[3][0] = 'D1'
+matriks2[3][1] = 'D2'
+matriks2[3][2] = 'D3'
+matriks2[3][3] = 'D4'
+matriks2[3][4] = 'D5'
+matriks2[4][0] = 'E1'
+matriks2[4][1] = 'E2'
+matriks2[4][2] = 'E3'
+matriks2[4][3] = 'E4'
+matriks2[4][4] = 'E5'
+
+matriks1 = []
+for i in range(b):
+    matriks1.append([])
+    for j in range(k):
+        matriks1[i].append(i)
+matriks1[0][0] = 'A1'
+matriks1[0][1] = 'A2'
+matriks1[0][2] = 'A3'
+matriks1[0][3] = 'A4'
+matriks1[0][4] = 'A5'
+matriks1[1][0] = 'B1'
+matriks1[1][1] = 'B2'
+matriks1[1][2] = 'B3'
+matriks1[1][3] = 'B4'
+matriks1[1][4] = 'B5'
+matriks1[2][0] = 'C1'
+matriks1[2][1] = 'C2'
+matriks1[2][2] = 'C3'
+matriks1[2][3] = 'C4'
+matriks1[2][4] = 'C5'
+matriks1[3][0] = 'D1'
+matriks1[3][1] = 'D2'
+matriks1[3][2] = 'D3'
+matriks1[3][3] = 'D4'
+matriks1[3][4] = 'D5'
+matriks1[4][0] = 'E1'
+matriks1[4][1] = 'E2'
+matriks1[4][2] = 'E3'
+matriks1[4][3] = 'E4'
+matriks1[4][4] = 'E5' 
+
+def tiketbioskop():
+    #Selamat Datang, mau pesan film apa
+    Df1 = 'Spiderman No Way Home'
+    Df2 = 'Batman'
+    Df3 = 'Sonic 2'
+    Df4 = 'Venom'
+    
+    print('=====Daftar Film=====')
+    print('1.',Df1)
+    print('2.',Df2)
+    print('3.',Df3)
+    print('4.',Df4)
+
+    while(True):
+        p = int(input('=-Masukkan Pilihan Film(1/2/3/4) : '))
+        if p == 1:
+            movie = Df1
+            break
+        elif p == 2:
+            movie = Df2
+            break
+        elif p == 3:
+            movie = Df3
+            break
+        elif p == 4:
+            movie = Df4
+            break
+        else:
+            print('Maaf untuk film yang anda pilih tidak tersedia')
+    Jam_tayang = ['12.30','14.45']
+    print('Jam yang tersedia:')
+    print(Jam_tayang)
+    while(True):
+        j = (input('=-Jam yang anda mau : '))
+        if j == (Jam_tayang[0]) and p == 1:
+            jam = (Jam_tayang[0])
+            tabel = matriks
+            break
+        elif j == (Jam_tayang[1]) and p == 1:
+            jam = (Jam_tayang[1])
+            tabel = matriks1
+            break
+        elif j == (Jam_tayang[0]) and p == 2:
+            jam = (Jam_tayang[0])
+            tabel = matriks2
+            break
+        elif j == (Jam_tayang[1]) and p == 2:
+            jam = (Jam_tayang[1])
+            tabel = matriks3
+            break
+        elif j == (Jam_tayang[0]) and p == 3:
+            jam = (Jam_tayang[0])
+            tabel = matriks4
+            break
+        elif j == (Jam_tayang[1]) and p == 3:
+            jam = (Jam_tayang[1])
+            tabel = matriks5
+            break
+        elif j == (Jam_tayang[0]) and p == 4:
+            jam = (Jam_tayang[0])
+            tabel = matriks6
+            break
+        elif j == (Jam_tayang[1]) and p == 4:
+            jam = (Jam_tayang[1])
+            tabel = matriks7
+            break
+        else:
+            print('Maaf untuk jam yang anda pilih tidak tersedia')
+    
+    while(True):
+        print('==Tempat duduk yang tersedia==')
+        for i in range(5):
+            print(tabel[i])    
+        seat = input('Silahkan pilih tempat duduk yang tersedia: ').split()
+        tiket = []
+        if 'A1' in seat and tabel[0][0] != 'X':
+            tabel[0][0] = 'X'
+            tiket.append('A1')
+        if 'A2' in seat and tabel[0][1] != 'X':
+            tabel[0][1] = 'X'
+            tiket.append('A2')
+        if 'A3' in seat and tabel[0][2] != 'X':
+            tabel[0][2] = 'X'
+            tiket.append('A3')
+        if 'A4' in seat and tabel[0][3] != 'X':
+            tabel[0][3] = 'X'
+            tiket.append('A4')
+        if 'A5' in seat and tabel[0][4] != 'X':
+            tabel[0][4] = 'X'
+            tiket.append('A5')  
+        if 'B1' in seat and tabel[1][0] != 'X':
+            tabel[1][0] = 'X'
+            tiket.append('B1')
+        if 'B2' in seat and tabel[1][1] != 'X':
+            tabel[1][1] = 'X'
+            tiket.append('A2')
+        if 'B3' in seat and tabel[1][2] != 'X':
+            tabel[1][2] = 'X'
+            tiket.append('B3')
+        if 'B4' in seat and tabel[1][3] != 'X':
+            tabel[1][3] = 'X'
+            tiket.append('B4')  
+        if 'B5' in seat and tabel[1][4] != 'X':
+            tabel[1][4] = 'X'
+            tiket.append('B5')
+        if 'C1' in seat and tabel[2][0] != 'X':
+            tabel[2][0] = 'X'
+            tiket.append('C1')
+        if 'C2' in seat and tabel[2][1] != 'X':
+            tabel[2][1] = 'X'
+            tiket.append('C2')
+        if 'C3' in seat and tabel[2][2] != 'X':
+            tabel[2][2] = 'X'
+            tiket.append('C3')
+        if 'C4' in seat and tabel[2][3] != 'X':
+            tabel[2][3] = 'X'
+            tiket.append('C4')
+        if 'C5' in seat and tabel[2][4] != 'X':
+            tabel[2][4] = 'X'
+            tiket.append('C5')
+        if 'D1' in seat and tabel[3][0] != 'X':
+            tabel[3][0] = 'X'
+            tiket.append('D1')
+        if 'D2' in seat and tabel[3][1] != 'X':
+            tabel[3][1] = 'X'
+            tiket.append('D2')
+        if 'D3' in seat and tabel[3][2] != 'X':
+            tabel[3][2] = 'X'
+            tiket.append('D3')
+        if 'D4' in seat and tabel[3][3] != 'X':
+            tabel[3][3] = 'X'
+            tiket.append('D4')
+        if 'D5' in seat and tabel[3][4] != 'X':
+            tabel[3][4] = 'X'
+            tiket.append('D5')
+        if 'E1' in seat and tabel[4][0] != 'X':
+            tabel[4][0] = 'X'
+            tiket.append('E1')
+        if 'E2' in seat and tabel[4][1] != 'X':
+            tabel[4][1] = 'X'
+            tiket.append('E2')
+        if 'E3' in seat and tabel[4][2] != 'X':
+            tabel[4][2] = 'X'
+            tiket.append('E3')
+        if 'E4' in seat and tabel[4][3] != 'X':
+            tabel[4][3] = 'X'
+            tiket.append('E4')  
+        if 'E5' in seat and tabel[4][4] != 'X':
+            tabel[4][4] = 'X'
+            tiket.append('E5')
+        if 'X' in seat:
+            print('Maaf tempat yang anda pilih sudah terisi!')
+            continue
+        break
+        
+
+
+    print('-Film\t:',movie)
+    print('-Jam\t:',jam)
+    print('-Seat\t:',seat)
+    print('===Terimakasih===\n')
+    lanjut = input('Ketik y jika ingin membeli tiket (y/n) : ')
+    if lanjut == 'y':
+        tiketbioskop()
+    else:
+        print('Terima Kasih!')
+
+
+print('Selamat datang!')
+pesan = input('Apakah kamu ingin membeli tiket(y/n)?: ')
+if pesan == 'y':
+    tiketbioskop()
+else:
+    print('Terima Kasih!')
